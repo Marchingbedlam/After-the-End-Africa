cba76221452ac90cbf46691cb19e483af4c5d41a
PaintThinner <53921031+PaintThinner@users.noreply.github.com>
2019-08-09 09:37:50 +1000

    Add files via upload

---
+++ africa/common/defines/am_defines.lua
@@ -0,0 +1,88 @@
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 2250
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_8 = 3100
+
+NDefines.NEngine.MISSING_SCRIPTED_SUCCESSOR_ERROR_CUTOFF_YEAR = 2670
+
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1000.0
+NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 900.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1100.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 648.0
+NDefines.NFrontend.CAMERA_START_X = 300.0
+NDefines.NFrontend.CAMERA_START_Y = 1650.0
+NDefines.NFrontend.CAMERA_START_Z = 100.0
+NDefines.NFrontend.CAMERA_END_X = 1000.0
+NDefines.NFrontend.CAMERA_END_Y = 1250.0
+NDefines.NFrontend.CAMERA_END_Z = 350.0
+
+NDefines.NFrontend.FRONTEND_POS_X = 1200.0
+NDefines.NFrontend.FRONTEND_POS_Y = 1450.0
+NDefines.NFrontend.FRONTEND_POS_Z = 350.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 1200.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 20.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1050.0
+
+NDefines.NFrontend.SETTINGS_POS_X = 900.0
+NDefines.NFrontend.SETTINGS_POS_Y = 500.0
+NDefines.NFrontend.SETTINGS_POS_Z = 950.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2000.0
+NDefines.NFrontend.SETTINGS_LOOK_Y = -300.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1600.0
+
+NDefines.NFrontend.MP_OPTIONS_POS_X = 2000.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 1200.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 250.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 800.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Y = -200.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1250.0
+
+NDefines.NFrontend.TUTORIAL_POS_X = 1408.0
+NDefines.NFrontend.TUTORIAL_POS_Y = 650.0
+NDefines.NFrontend.TUTORIAL_POS_Z = -500.0
+NDefines.NFrontend.TUTORIAL_LOOK_X = 1408.0
+NDefines.NFrontend.TUTORIAL_LOOK_Y = 0.0
+NDefines.NFrontend.TUTORIAL_LOOK_Z = 1300.0
+
+NDefines.NFrontend.CREDITS_POS_X = 1000.0
+NDefines.NFrontend.CREDITS_POS_Y = 200.0
+NDefines.NFrontend.CREDITS_POS_Z = 1308.0
+NDefines.NFrontend.CREDITS_LOOK_X = 900.0
+NDefines.NFrontend.CREDITS_LOOK_Y = 0.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 1450.0
+
+NDefines.NEndGame.DYN1_SCORE = 100000
+NDefines.NEndGame.DYN1_ID = 1851041
+NDefines.NEndGame.DYN2_SCORE = 90000
+NDefines.NEndGame.DYN2_ID = 1782000
+NDefines.NEndGame.DYN3_SCORE = 80000
+NDefines.NEndGame.DYN3_ID = 1776013
+NDefines.NEndGame.DYN4_SCORE = 70000
+NDefines.NEndGame.DYN4_ID = 1779000
+NDefines.NEndGame.DYN5_SCORE = 60000
+NDefines.NEndGame.DYN5_ID = 1795000
+NDefines.NEndGame.DYN6_SCORE = 50000
+NDefines.NEndGame.DYN6_ID = 1491003
+NDefines.NEndGame.DYN7_SCORE = 40000
+NDefines.NEndGame.DYN7_ID = 1650002
+NDefines.NEndGame.DYN8_SCORE = 30000
+NDefines.NEndGame.DYN8_ID = 1789000
+NDefines.NEndGame.DYN9_SCORE = 20000
+NDefines.NEndGame.DYN9_ID = 1889009
+NDefines.NEndGame.DYN10_SCORE = 15000
+NDefines.NEndGame.DYN10_ID = 1519000
+NDefines.NEndGame.DYN11_SCORE = 10000
+NDefines.NEndGame.DYN11_ID = 1788046 
+NDefines.NEndGame.DYN12_SCORE = 7500
+NDefines.NEndGame.DYN12_ID = 1796003
+NDefines.NEndGame.DYN13_SCORE = 5000
+NDefines.NEndGame.DYN13_ID = 1790001
+NDefines.NEndGame.DYN14_SCORE = 2000
+NDefines.NEndGame.DYN14_ID = 1491901
+NDefines.NEndGame.DYN15_SCORE = 1000
+NDefines.NEndGame.DYN15_ID = 2001034
+
+NDefines.NLearningScenario.START_YEAR = 2666
+NDefines.NLearningScenario.START_MONTH = 1
+NDefines.NLearningScenario.START_DAY = 1
+NDefines.NLearningScenario.START_CHARACTER = 1788001
